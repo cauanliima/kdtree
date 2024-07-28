f5f0b4ebd151e2e01d7d0d84cbfb41c3356bffeb
Cauan <cauanmarcelo@hotmail.com>
2024-07-28 12:40:12 -0300

    ajustes

--- trab/kdtree.cpp
+++ trab/kdtree.cpp
@@ -3,31 +3,27 @@
 KDTreeGreenhouse::KDTreeGreenhouse(data_type *data, array_size n_datapoints,
                                    int n_components)
     : n_datapoints{n_datapoints}, n_components{n_components} {
-  bool should_delete_data = false;
-
   if (data == nullptr) {
     throw std::invalid_argument("Received a null dataset.");
   }
 
   std::vector<DataPoint> data_points =
       as_data_points(data, this->n_datapoints, this->n_components);
-  // 1D representation of our KDTree, or nullptr (if not main process)
+  // 1D representation of our KDTree
   data_type *tree = grow_kd_tree(data_points);
 
-  // tree is nullptr if the branch assigned to this process was empty
   if (tree != nullptr)
     grown_kd_tree =
         convert_to_knodes(tree, grown_kdtree_size, this->n_components, 0, 1, 0);
   else {
     grown_kd_tree = new KNode<data_type>();
   }
-
-  if (should_delete_data)
-    delete[] data;
 }
 
 data_type *KDTreeGreenhouse::grow_kd_tree(std::vector<DataPoint> &data_points) {
-#pragma omp parallel default(none) shared(n_parallel_workers, max_parallel_depth, surplus_workers, std::cout, growing_tree, tree_size, data_points)
+#pragma omp parallel default(none)                                             \
+    shared(n_parallel_workers, max_parallel_depth, surplus_workers, std::cout, \
+           growing_tree, tree_size, data_points)
   {
 #pragma omp single
     {
@@ -41,13 +37,10 @@ data_type *KDTreeGreenhouse::grow_kd_tree(std::vector<DataPoint> &data_points) {
                 << " parallel workers." << std::endl;
 #endif
 
-      // we want to store the tree (in a temporary way) in an array whose size
-      // is a powersum of two
       tree_size = powersum_of_two(n_datapoints, true);
       growing_tree = new std::optional<DataPoint>[tree_size];
 
-      int starting_region_width;
-      starting_region_width = 1;
+      int starting_region_width = 1;
 
       build_tree_single_core(data_points.begin(), data_points.end(), 0,
                              starting_region_width, 0, 0);
@@ -65,8 +58,6 @@ void KDTreeGreenhouse::build_tree_single_core(
     std::vector<DataPoint>::iterator end_data_point, int depth,
     array_size region_width, array_size region_start_index,
     array_size branch_starting_index) {
-  // this is equivalent to say that there is at most one data point in the
-  // sequence
   if (first_data_point + 1 != end_data_point) {
     int dimension = select_splitting_dimension(depth, n_components);
     array_size split_point_idx =
@@ -81,8 +72,6 @@ void KDTreeGreenhouse::build_tree_single_core(
     region_start_index_left = region_start_index_right =
         region_start_index + region_width;
 
-    // the width of the next level will be twice the width of the current
-    // level
     region_width *= 2;
 
     branch_starting_index *= 2;
@@ -90,27 +79,26 @@ void KDTreeGreenhouse::build_tree_single_core(
     branch_start_index_right = branch_starting_index + 1;
     depth += 1;
 
-#pragma omp task final(depth > max_parallel_depth + 1 || (depth == max_parallel_depth + 1 && get_rank() >= surplus_workers))
+    std::vector<DataPoint>::iterator right_branch_first_point =
+        first_data_point + split_point_idx + 1;
+#pragma omp task
     {
 #ifdef DEBUG
-#ifdef USE_OMP
       std::cout << "Task assigned to thread " << omp_get_thread_num()
                 << std::endl;
 #endif
-#endif
-      // right
-      build_tree_single_core(first_data_point + split_point_idx + 1, end_data_point, depth,
+      build_tree_single_core(right_branch_first_point, end_data_point, depth,
                              region_width, region_start_index_right,
                              branch_start_index_right);
     }
-    // left
     if (split_point_idx > 0)
-      build_tree_single_core(first_data_point, first_data_point + split_point_idx, depth, region_width,
-                             region_start_index_left, branch_start_index_left);
-
+      build_tree_single_core(first_data_point, right_branch_first_point - 1,
+                             depth, region_width, region_start_index_left,
+                             branch_start_index_left);
 #pragma omp taskwait
   } else {
     growing_tree[region_start_index + branch_starting_index].emplace(
         DataPoint(std::move(*first_data_point)));
   }
 }
+
