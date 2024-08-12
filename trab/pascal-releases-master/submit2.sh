15ab8aca1880882cdc21b398853dfe82f714609c
Cauan <cauanmarcelo@hotmail.com>
2024-08-12 13:41:46 -0300

    ajuste submit

--- trab/pascal-releases-master/submit2.sh
+++ trab/pascal-releases-master/submit2.sh
@@ -8,4 +8,4 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --time=0-0:90
 #SBATCH --exclusive
-pascalanalyzer  -t aut -c 1 -i ../../benchmark/5M,../../benchmark/10M,../../benchmark/20M,../../benchmark/40M,../../benchmark/80M,../../benchmark/160M -r 1 -o out.json ./tree_serial.x
+pascalanalyzer  -t aut -c 1,2,4,8,16,32 -i ../../benchmark/5M,../../benchmark/10M,../../benchmark/20M,../../benchmark/40M,../../benchmark/80M,../../benchmark/160M -r 1 -o out.json ./tree_omp.x
